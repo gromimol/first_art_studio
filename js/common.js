490f696f0ba88af7c1c9a6cbae30d528b0ec9958
Grom <gromimol@ukr.net>
2019-09-24 17:20:33 +0300

    меню

--- js/common.js
+++ js/common.js
@@ -2,7 +2,16 @@ $(document).ready(function () {
 	
 	// menu btn
 	$('.menu-btn').on('click',function () {
+
+		$('body').toggleClass('noscroll');
 		$(this).toggleClass('active');
-	})
+		$('.menu-block').toggleClass('active');
+	});
+
+	// slider
+	$('.case-slider').slick({
+		appendArrows: '.case-slider-arrows',
+		variableWidth: true
+	});
 
 })
